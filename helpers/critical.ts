4ea30d7e3f88f9c379a3dc4b309257ea487b9fad
Anton <frontendevds@gmail.com>
2022-03-27 11:14:28 +0300

    small fix animation

--- helpers/critical.ts
+++ helpers/critical.ts
@@ -2,17 +2,12 @@
 // Set as close as possible to html tag
 export const criticalCSS = `
 html[data-theme='light'] {
-  --bg-clr: hsl(39, 43%, 90%);
-  --primary-clr: hsl(252, 10%, 10%);
+  background-color: hsl(39, 43%, 90%);
+  color: hsl(252, 10%, 10%);
 }
 html[data-theme='dark'] {
-  --bg-clr: hsl(252, 10%, 10%);
-  --primary-clr: hsl(39, 43%, 90%);
-}
-html {
-  background-color: var(--bg-clr);
-  color: var(--primary-clr);
-  transition: all .5s ease;
+  background-color: hsl(252, 10%, 10%);
+  color: hsl(39, 43%, 90%);
 }
   `;
 // Check or set theme from the LocalStorage
--- pages/404.tsx
+++ pages/404.tsx
@@ -15,34 +15,6 @@ const Custom404: NextPage = () => {
       <Head>
         <meta name="description" content="page not found" />
         <title>page not found</title>
-        {/* <style
-          id="___critical-css"
-          dangerouslySetInnerHTML={{
-            __html: `
-                html[data-theme='light'] {
-                  --bg-clr: hsl(39, 43%, 90%);
-                  --primary-clr: hsl(252, 10%, 10%);
-                  --secondary-clr: hsl(341, 53%, 33%);
-                  --card-clr: hsl(39, 42%, 87%);
-                  --card-shadow-clr: hsl(38, 12%, 70%);
-                  --card-extra-clr: hsl(38, 12%, 70%);
-                }
-                html[data-theme='dark'] {
-                  --bg-clr: hsl(252, 10%, 10%);
-                  --primary-clr: hsl(39, 43%, 90%);
-                  --secondary-clr: hsl(162, 47%, 66%);
-                  --card-clr: hsl(240, 3%, 12%);
-                  --card-shadow-clr: hsl(0, 0%, 0%);
-                  --card-extra-clr: hsl(220, 6%, 19%);
-                }
-                html {
-                  background-color: var(--bg-clr);
-                  color: var(--primary-clr);
-                  transition: all .5s ease;
-                }
-              `,
-          }}
-        /> */}
       </Head>
       <ErrorPage>
         <ErrorImg404 />
