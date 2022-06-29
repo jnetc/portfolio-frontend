bcf59882448963d71b7d434c2686258c3cb97435
Anton <frontendevs@gmail.com>
2022-06-29 13:32:19 +0300

    increase message limit  to 1500 characters

--- components/modal/modal-email-form/ModalTextarea.tsx
+++ components/modal/modal-email-form/ModalTextarea.tsx
@@ -8,12 +8,14 @@ import { modalTextarea } from '@Helpers/localization';
 // Style
 import emailform from './emailform.module.css';
 
-export const ModalTextarea = ({
-  nameInput,
-  handler,
-  value,
-}: TextareaFromType) => {
+export const ModalTextarea = ({ nameInput, handler, value }: TextareaFromType) => {
   const { lang } = useContextLanguage();
+  const maxLength = 1500;
+
+  const valueTextArea = {
+    ru: `длина сообщения ${value.length} из 1500`,
+    en: `length message ${value.length} of 1500`,
+  };
 
   const getValue = (ev: ChangeEvent<HTMLTextAreaElement>) => {
     handler(ev.currentTarget.value);
@@ -26,19 +28,22 @@ export const ModalTextarea = ({
         name={nameInput}
         rows={3}
         required
-        maxLength={500}
+        maxLength={maxLength}
         onChange={getValue}
         value={value}
       />
       <label
         className={
-          value
-            ? `${emailform.placeholder} ${emailform.not_empty}`
-            : `${emailform.placeholder}`
+          value ? `${emailform.placeholder} ${emailform.not_empty}` : `${emailform.placeholder}`
         }
       >
         {modalTextarea[nameInput][lang]}
       </label>
+      <span
+        className={`${emailform.message_limit} ${value.length === 1500 ? emailform.limit : ''}`}
+      >
+        {valueTextArea[lang]}
+      </span>
     </fieldset>
   );
 };
