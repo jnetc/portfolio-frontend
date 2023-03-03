ef4e51a42dd58fcd8a2828bbc7b0e507fe869aa0
Anton <frontendevs@gmail.com>
2023-03-03 14:58:54 +0200

    added canonical href/ en link to the HEAD

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,28 +21,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="UI/UX designer, UI designer, web designer, fullstack designer, UI developer, frontend developer, fullstack developer, react developer, vue developer, javascript developer, software engineer, frontend devaaja, UI devaaja, web devaaja, frontend kehittäjä, web kehittäjä, UI kehittäjä, vue kehittäjä, react kehittäjä, UI suunnittelija, fullstack suunnittelija, web suunnittelija, graafinen suunnittelija, UI/UX дизайнер, UI дизайнер, веб дизайнер, fullstack дизайнер, веб разработчик, фронтенд разработчик, frontend разработчик, web разработчик"
         />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: light)"
-          content="#f0e9db"
-        />
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#18171c"
-        />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f0e9db" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#18171c" />
 
+        <link rel="canonical" href="https://devan.fi" />
         <link rel="alternate" href="https://devan.fi/ru" hrefLang="ru" />
+        <link rel="alternate" href="https://devan.fi/en" hrefLang="en" />
         <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
         <link rel="manifest" href="/manifest.webmanifest" />
         {/* Apple */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="devan.fi" />
-        <link
-          rel="apple-touch-icon"
-          sizes="192x192"
-          href="/icons/icon-192x192.png"
-        />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
       </Head>
       <Component {...pageProps} />
     </>
