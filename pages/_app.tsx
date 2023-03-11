52ee71532fb2205683c7535c91c5ab2b08d9aff4
Anton <frontendevs@gmail.com>
2023-03-11 16:59:59 +0200

    added google tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,6 +41,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTIC}`} />
 
+      <Script id="google-tags" strategy="afterInteractive">{`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NPDFKFS');`}</Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
