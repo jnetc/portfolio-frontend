06b0fff20f1cf63cb8d4d60c63847804f8d6681d
Anton <frontendevs@gmail.com>
2023-03-21 14:52:53 +0200

    added observer hook, video files, changed css properties in portfolio folder

--- components/portfolio/RecentProject.tsx
+++ components/portfolio/RecentProject.tsx
@@ -4,9 +4,10 @@ import { Description } from '@Portfolio/Description';
 import { Tags } from '@Portfolio/tags/Tags';
 import { Links } from '@Portfolio/Links';
 import { Date } from '@Portfolio/Date';
-import Img from '@Img';
+// import Img from '@Img';
 // Hooks
 import { useContextLanguage } from '@Hooks/useContextLanguage';
+import { useTransitionObserver } from '@Hooks/useTransitionObserver';
 // Helpers
 import { dataTranslate } from '@Helpers/functions';
 // Type
@@ -18,12 +19,9 @@ export const RecentProject = ({ data, position }: { data: ProjectType; position:
   const { lang } = useContextLanguage();
   const projectCompleted = dataTranslate(data.project_completed, lang);
 
+  useTransitionObserver('transition');
   return (
-    <section
-      className={`grid mob-right-pad grid-12 ${
-        position ? `${style.left} project-margin` : `${style.right} project-margin`
-      }`}
-    >
+    <section className={`grid mob-right-pad grid-12 ${position ? `${style.left} project-margin` : `${style.right} project-margin`}`}>
       <article className={`${style.project} ${style.recent}`}>
         <Title title={data.project_title} />
         <Date date={projectCompleted} />
@@ -31,15 +29,14 @@ export const RecentProject = ({ data, position }: { data: ProjectType; position:
         <Tags tags={data.tags} />
         <Links github={data.github_href} page={data.page_href} figma={data.figma_href} />
       </article>
-      <a
-        className={style.image}
-        href={data.page_href}
-        title={data.page_href}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Img pathImage={data.poster} objectFit="fill" alt={data.project_title} />
+      <a className={`${style.video} transition`} href={data.page_href} title={data.page_href}>
+        <video muted loop poster="./images/poster_timamih.png">
+          <source src="./video/timamih.webm" type="video/webm" />
+        </video>
       </a>
+      {/* <a className={style.video} href={data.page_href} title={data.page_href} target="_blank" rel="noreferrer">
+        <Img pathImage={data.poster} objectFit="fill" alt={data.project_title} />
+      </a> */}
     </section>
   );
 };
