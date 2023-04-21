75bc3c48a275a044e35d1ba5c46b65610e32c208
Anton <frontendevs@gmail.com>
2023-04-21 13:48:47 +0300

    update favicon meta

--- helpers/seo-schema.ts
+++ helpers/seo-schema.ts
@@ -4,25 +4,31 @@ import type { MainType } from '@Types';
 export function createSchema(data: MainType) {
   const schema = {
     '@context': 'https://schema.org',
-    '@type': 'Person',
-    name: 'Anton',
-    jobTitle: 'UI / Front-end developer',
+    '@type': 'WebSite',
     url: 'https://www.devan.fi/',
-    sameAs: [data.stackoverflow_href, data.github_href],
-    brand: {
-      '@type': 'Brand',
-      logo: 'https://www.devan.fi/icons/icon-512x512.png',
-    },
-    contactPoint: {
-      '@type': 'ContactPoint',
-      areaServed: 'FI',
-      availableLanguage: ['English', 'Finnish', 'Russian'],
-    },
-    address: {
-      '@type': 'PostalAddress',
-      addressLocality: 'Espoo',
-      addressRegion: 'Uusimaa',
-      addressCountry: 'Finland',
+    name: 'devan.fi',
+    description: 'UI/UX designer and Front-end developer portfolio',
+    author: {
+      '@type': 'Person',
+      name: 'Anton',
+      jobTitle: 'UI / Front-end developer',
+      url: 'https://www.devan.fi/',
+      sameAs: [data.stackoverflow_href, data.github_href],
+      brand: {
+        '@type': 'Brand',
+        logo: 'https://www.devan.fi/icons/favicon-512x512.png',
+      },
+      contactPoint: {
+        '@type': 'ContactPoint',
+        areaServed: 'FI',
+        availableLanguage: ['English', 'Finnish', 'Russian'],
+      },
+      address: {
+        '@type': 'PostalAddress',
+        addressLocality: 'Espoo',
+        addressRegion: 'Uusimaa',
+        addressCountry: 'Finland',
+      },
     },
   };
   return schema;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-// import Script from 'next/script';
+import Script from 'next/script';
 import '@Styles/globals.css';
 
 export const GOOGLE_ANALYTIC = process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC as string;
@@ -12,6 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="HandheldFriendly" content="true" />
         <meta name="MobileOptimized" content="width" />
+
+        <meta name="robots" content="index,follow"/>
 
         {/* App PWA */}
         <meta name="application-name" content="devan.fi" />
@@ -30,19 +32,33 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="alternate" href="https://www.devan.fi/en/" hrefLang="en" />
 
         {/* Favicon */}
-        <link rel="shortcut icon" href="/icons/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/icons/favicon-48x48.png" type="image/png" sizes="48x48" />
+        <link rel="icon" href="/icons/favicon-96x96.png" type="image/png" sizes="96x96" />
         <link rel="icon" href="/icons/favicon-192x192.png" type="image/png" sizes="192x192" />
         <link rel="icon" href="/icons/favicon-512x512.png" type="image/png" sizes="512x512" />
-        <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="/icons/favicon-180×180.png" type="image/png" sizes="180×180" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/icons/favicon.ico" type="image/x-icon"/>
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180x180.png" type="image/png" sizes="180x180"/>
+        <link rel="apple-touch-icon-precomposed" href="/docs/favicon/apple-touch-icon-57x57.png" sizes="57x57"/>
+
 
         {/* Others */}
         <meta name="format-detection" content="telephone=no, address=no" />
         <meta name="rating" content="General" />
         <meta name="generator" content="Next.js" />
-        <meta name="google" content="notranslate" />
       </Head>
+
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTIC}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GOOGLE_ANALYTIC}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
